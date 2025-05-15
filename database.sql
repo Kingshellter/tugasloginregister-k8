c670c459ef5ca96d519c25178f4971d07987a296
M. Maulana Gian Pranaja <bandaibadak20@gmail.com>
2025-05-15 17:28:07 +0700

    update sql

--- database.sql
+++ database.sql
@@ -13,6 +13,7 @@ CREATE table users (
 );
 
 -- Insert admin default
-INSERT INTO users (username, email, password, role) VALUES ('tes','tes@uns.com','tes123','admin')
-INSERT INTO users (username, email, password, role) VALUES ('iniadmin','admin@uns.com','admin123','admin')
-
+INSERT INTO users (username, email, password, role) VALUES ('tes','tes@uns.com','$2y$12$QM4SuIrmdbyTSTRELuJDK.45xFT66Ml0Czj/RhY9r.KkDMKCzgGJm','admin')
+/*untuk password admin tes adalah tes123*/
+INSERT INTO users (username, email, password, role) VALUES ('iniadmin','admin@uns.com','$2y$12$FcNxol3DPtg06TC/.8TMwuvX/IMOiUsVJwsS4UOAY7rMxpbLuVJrG','admin')
+/*untuk password admin iniadmin adalah admin123*/
